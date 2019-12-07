69089242126756f9c7e7e1390bd7bc4504f9aa30
baswojuvishal <58410813+baswojuvishal@users.noreply.github.com>
2019-12-07 12:31:33 +0530

    Add files via upload

---
+++ conditional op2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+int a= 10,b=40,c=8900,d;
+d=(a>b&&a>c)?a:b>c?b:c;
+printf("the biggest number is %d",d );
+return 0;
+}
+
---
+++ even odd.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	if(a%2==0)
+	printf("given number is even");
+	else
+	printf("given number is odd");
+		}
